f8073416108ed609d9b070c397eaec9861bd50b8
«steven» <ssoriano@elpuig.xeill.net>
2026-04-29 10:55:25 +0200

    script de spike_ball

--- escenas/enemigos/spike_ball.gd
+++ escenas/enemigos/spike_ball.gd
@@ -1,15 +1,36 @@
 extends Node2D
 
+
 var floorDetected = false
-# Called when the node enters the scene tree for the first time.
+var safeTimeOut = false
+var rayCastInitValue = 36 
+
+
+
 func _ready():
-	pass # Replace with function body.
+	$raycast_floor_detection.target_position.y = rayCastInitValue
+	$safeTime.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if not floorDetected:
-		$raycast_floor_detection.target_position.y +=5
+	if not floorDetected && safeTimeOut:
+		$raycast_floor_detection.target_position.y += 6
 		if $raycast_floor_detection.is_colliding():
 			floorDetected = true
-			print("Suelo Detectado", $raycast_floor_detection.target_position.y)
+			$raycast_floor_detection.target_position.y -= 6
+			init_spikeball()
+
+
+func init_spikeball():
+	var numberOfChains = ($raycast_floor_detection.target_position.y - rayCastInitValue) / 6
+	$SpikedBall.position.y += (numberOfChains * 6)
+	for i in range(numberOfChains):
+		var newRing = preload("res://escenas/enemigos/One_chain.tscn").instantiate()
+		newRing.position = Vector2(0,(6*i))
+
+	print(numberOfChains)
+
+
+func _on_safe_time_timeout():
+	safeTimeOut = true

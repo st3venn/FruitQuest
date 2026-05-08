ff6384e129e7cbce91dd4f0410cc037ac3b58585
st3venn <ssoriano@elpuig.xeill.net>
2026-05-08 16:39:36 +0200

    barra de vida

---
+++ escenas/pantallas/playerinfo.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if get_parent().has_node("ninjacat"):
+		$health_ProgressBar.value = get_parent().get_node("ninjacat").health
+
+func _process(delta):
+	$health_ProgressBar.value = get_parent().get_node("ninjacat").health

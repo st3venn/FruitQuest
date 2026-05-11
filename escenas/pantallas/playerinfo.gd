8d09842c30c0b3bd72a5637f58d399e4740105c6
steven <ssoriano@elpuig.xeill.net>
2026-05-11 11:36:28 +0200

    barra de vida

--- escenas/pantallas/playerinfo.gd
+++ escenas/pantallas/playerinfo.gd
@@ -4,6 +4,7 @@ extends CanvasLayer
 func _ready():
 	if get_parent().has_node("ninjacat"):
 		$health_ProgressBar.value = get_parent().get_node("ninjacat").health
-
+		$FruitPointLabel.text = "Frutas:" + str(get_parent().get_node("ninjacat").fruitCount)
 func _process(delta):
 	$health_ProgressBar.value = get_parent().get_node("ninjacat").health
+	$FruitPointLabel.text = "Frutas:" + str(get_parent().get_node("ninjacat").fruitCount)

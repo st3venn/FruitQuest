47c5ed2617e9e37c8299a1376d80effe9234578b
steven <ssoriano@elpuig.xeill.net>
2026-05-12 19:12:32 +0200

    nuevo

--- escenas/pantallas/playerinfo.gd
+++ escenas/pantallas/playerinfo.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 @onready var menuPopUp : Node2D = $MenuPopUp
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	inicia_sliders()
 	menuPopUp.visible = false
 	if get_parent().has_node("ninjacat"):
 		$health_ProgressBar.value = get_parent().get_node("ninjacat").health
@@ -24,9 +25,14 @@ func _on_reiniciar_pressed():
 	get_tree().paused = false
 	get_tree().reload_current_scene()
 
-func _on_sonido_pressed():
-	pass # Replace with function body.
 
+func _on_music_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(1, value - 100)
 
-func _on_musica_pressed():
-	pass # Replace with function body.
+func _on_sonido_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(2, value - 100)
+	
+func inicia_sliders():
+	for slider in menuPopUp.get_children():
+		if slider is HSlider:
+			slider.value = 100

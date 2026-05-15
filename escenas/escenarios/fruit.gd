5ca290da085695cfc09938bd7740432bcb44fa11
steven <ssoriano@elpuig.xeill.net>
2026-05-15 23:38:11 +0200

    ultimo retoques

--- escenas/escenarios/fruit.gd
+++ escenas/escenarios/fruit.gd
@@ -8,41 +8,38 @@ var collected = false
 		fruitType = value
 		$animations.animation = fruitType
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	if not Engine.is_editor_hint():
 		print("Frutas totales: ", Global.total_fruits)
 		$animations.play(fruitType)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
 func _process(delta):
 	pass
 
 func _on_area_collect_body_entered(body):
+
 	if collected:
 		return
-	
+
+	if not body.is_in_group("player"):
+		return
+
 	collected = true
-	
+
 	Global.collected_fruits += 1
 	print("Frutas recogidas: ", Global.collected_fruits)
-	
-	if Global.collected_fruits >= Global.total_fruits:
-		Global.puntuacion = body.fruitCount
-		Global.frutas_recogidas = Global.collected_fruits
-		Global.current_scene = get_tree().current_scene.scene_file_path
-		if Global.current_level == 3:
-			get_tree().change_scene_to_file("res://escenas/MENU/victoria.tscn")
-		else:
-			get_tree().change_scene_to_file("res://escenas/MENU/nivelcompletado.tscn")
-	
+
 	if body.has_method("collectFruit"):
 		body.collectFruit(fruitType)
-	
-	$animations.play("collected")
 
- 	
+	# guardar datos del jugador (ahora seguro)
+	Global.puntuacion = body.fruitCount
+	Global.frutas_recogidas = Global.collected_fruits
+	Global.current_scene = get_tree().current_scene.scene_file_path
+
+	$animations.play("collected")
 
 
 func _on_animations_animation_finished():
-	self.queue_free()
+	queue_free()
--- escenas/pantallas/siguientenivel.gd
+++ escenas/pantallas/siguientenivel.gd
@@ -1,27 +1,34 @@
 extends Area2D
 
-@export var next_scene_path: String
-
 var can_pass = false
-
 
 func _on_body_entered(body):
 
-	if body.name == "ninjacat":
+	if body.name != "ninjacat":
+		return
 
-		print(Global.collected_fruits)
-		print(Global.total_fruits)
+	if Global.total_fruits <= 0:
+		return
 
-		if Global.collected_fruits == Global.total_fruits:
-
-			can_pass = true
-			change_scene()
+	if Global.collected_fruits == Global.total_fruits:
+		can_pass = true
+		change_scene()
 
 
 func change_scene():
 
-	if can_pass:
-		Global.total_fruits = 0
-		Global.collected_fruits = 0
+	if not can_pass:
+		return
 
-		get_tree().change_scene_to_file(next_scene_path)
+	Global.total_fruits = 0
+	Global.collected_fruits = 0
+
+	call_deferred("_go_next")
+
+
+func _go_next():
+
+	if get_tree().current_scene.scene_file_path.ends_with("pantalla3.tscn"):
+		get_tree().change_scene_to_file("res://escenas/MENU/victoria.tscn")
+	else:
+		get_tree().change_scene_to_file("res://escenas/MENU/nivelcompletado.tscn")
